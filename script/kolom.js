ed90e9e6dafacb60909b988bec8a24e80160720b
BintangRF <bintangrama777@gmail.com>
2023-10-21 12:40:23 +0700

    perbaruan

--- script/kolom.js
+++ script/kolom.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
   fetch("kolom1.html")
     .then((response) => response.text())
     .then((kolomHTML) => {
-      document.getElementById("kolom-section").innerHTML = kolomHTML;
+      document.getElementById("kolom1-section").innerHTML = kolomHTML;
     });
 });
