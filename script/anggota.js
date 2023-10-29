8ab38ae0c931f45a55df4bdba056a6bc8c5e0d09
BintangRF <bintangrama777@gmail.com>
2023-10-29 16:16:21 +0700

    baru

--- script/anggota.js
+++ script/anggota.js
@@ -1,6 +1,6 @@
 // get hero
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("html/anggota.html")
+  fetch("html/anggota2.html")
     .then((response) => response.text())
     .then((anggotaHTML) => {
       document.getElementById("anggota-section").innerHTML = anggotaHTML;
